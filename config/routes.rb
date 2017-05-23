24bbfb20918e42f9fd94426c2f816b9c9b1d383b
Mike DeBerry <mik3.de1@gmail.com>
2017-05-22 21:24:43 -0400

    this is the second update of the night

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+	
+  get 'likes/create'
+
   devise_for :users
-  resources :tweets
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :tweets do
+  	resources :like
+  end
+  
+  resources :relationships
+  
+  get 'profiles/:id' => 'profiles#show', as: :profile
+  get 'profiles'  => 'profiles#index'
+  
+  root  "tweets#index"
+  
 end
